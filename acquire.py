58c3b21ae44356f3a518593769aee3b0d3769ca5
Yongliang Shi <yongliang.michael.shi@gmail.com>
2021-03-16 14:37:26 -0500

    data acquisition: define a function to acquire stock data

---
+++ acquire.py
@@ -0,0 +1,39 @@
+# General Library
+import numpy as np
+import pandas as pd
+
+# 
+import pandas_datareader.data as web
+
+# Create the functions to download the stock
+
+def acquire_stock_data(stocks, source, start_date, end_date):
+    '''
+    Return the adjusted closing prices of the interested stocks.
+    Parameters: 
+        - stocks(list): a list of the stock name
+        - source(str): specify where the data is downloaded.
+        - start_date(str): the start date in standard format
+        - end_date(str): the end date in standard format
+    '''
+    data = web.DataReader(stocks, data_source=source, start=start_date, end=end_date)['Adj Close']
+    data.columns = stocks
+    return data
+
+def load_jane_street_train():
+    '''
+    Return a dataframe that contains 
+    '''
+    # Load the train dataset by default
+    df_train = pd.read_csv("Database/train.csv")
+    # Create an iterator containing the names of the float64 columns
+    colsf64 = df_train.select_dtypes(include='float64').columns
+    # Create an iterator containing the names of the int64 columns
+    colsi64 = df_train.select_dtypes(include='int64').columns
+    # Create the dictionary to define the dtypes of the columns
+    mapperf32 = {col: np.float32 for col in colsf64}
+    mapperi32 = {col: np.int32 for col in colsi64}
+    # Cast the df_train to the defined dtypes
+    df_train = df_train.astype(mapperf32)
+    df_train = df_train.astype(mapperi32)
+    return df_train
